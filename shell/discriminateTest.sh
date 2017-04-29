c044fea769aae165ad41985ff13c618974de6b0b
javipus <javier.prietop@estudiante.uam.es>
2017-04-29 18:48:57 +0200

    removed unnecessary shell files

--- shell/discriminateTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-g++ -I /home/lib/armadillo/usr/local/include -L /home/lib/armadillo/usr/local/lib64 -larmadillo ../src/discriminateTest.cpp -o ../bin/discriminateTest.x -O3
-LD_LIBRARY_PATH=/home/lib/armadillo/usr/local/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
-../bin/discriminateTest.x
--- shell/experiment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-g++ -I /home/lib/armadillo/usr/local/include -L /home/lib/armadillo/usr/local/lib64 -larmadillo ../src/experiment.cpp -o ../bin/experiment.x
-LD_LIBRARY_PATH=/home/lib/armadillo/usr/local/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH
-../bin/experiment.x
